355424e4d8b904f50093bc8760ccf83b2b92ac10
Alex Massey <rsjankis@gmail.com>
2020-12-05 16:10:25 -0600

    Initial commit

---
+++ Day_5/Day_5_Part_1.ps1
@@ -0,0 +1,59 @@
+$InputData = Get-Content $PSScriptRoot\input.txt
+
+$SeatIDList = @()
+
+foreach ($item in $InputData){
+    $RowMax = 127
+    $RowMin = 0
+    $ColMax = 7
+    $ColMin = 0
+    $RowData = $item.ToCharArray()[0..6]
+    $ColData = $item.ToCharArray()[7..9]
+    $Row = 0
+    $Col = 0
+
+    foreach ($char in $RowData){
+        $Half = [Math]::Round(($RowMax-$RowMin)/2, [System.MidpointRounding]::AwayFromZero)
+        if ($char -eq "F"){
+            if ($Half -eq 1){
+                $Row = $RowMin
+            } else {
+                $RowMax = [Math]::Floor($RowMax - $Half)
+            }
+        }
+        elseif ($char -eq "B") {
+            if ($Half -eq 1){
+                $Row = $RowMax
+            } else{
+                $RowMin = [Math]::Floor($RowMin + $Half)
+            }
+        }
+    }
+
+    #Write-Host "Row: $Row" -fore green
+
+    foreach ($char in $ColData){
+        $Half = [Math]::Round(($ColMax-$ColMin)/2, [System.MidpointRounding]::AwayFromZero)
+        if ($char -eq "L"){
+            if ($Half -eq 1){
+                $Col = $ColMin
+            } else {
+                $ColMax = [Math]::Floor($ColMax - $Half)
+            }
+        }
+        elseif ($char -eq "R") {
+            if ($Half -eq 1){
+                $Col = $ColMax
+            } else{
+                $ColMin = [Math]::Floor($ColMin + $Half)
+            }
+        }
+    }
+    #Write-Host "Col: $Col" -ForegroundColor Green
+    $SeatID = $Row * 8 + $Col
+
+    $SeatIDList += $SeatID
+}
+
+$HighestSeatID = ($SeatIDList | Sort-Object )[-1]
+Write-Output "Highest Seat ID: $HighestSeatID"
