215c337e6038134e164548fbda4d5c3eaf6e9639
Alex Massey <rsjankis@gmail.com>
2020-12-05 00:39:13 -0600

    Optimized execution time from 21.5s to 1.4s

--- Day_1/Day_1.ps1
+++ Day_1/Day_1.ps1
@@ -1,24 +1,20 @@
-$input_nums= Get-Content .\input_numbers.txt
+$input_nums = Get-Content $PSScriptRoot\input_numbers.txt
 
 foreach ($number1 in $input_nums){
     foreach ($number2 in $input_nums){
-        if ($number1 -eq $number2){
+        if ($number1 -eq $number2) {
             #skip when same number is found in inner loop
             continue
-        }
-        foreach ($number3 in $input_nums){
-            if ($number2 -eq $number3){
-                #skip when same number is found in inner loop
-                continue
+        } else{
+            $remainder = 2020 - [int]$number1 - [int]$number2
+            if ($input_nums -contains $remainder){
+                Write-Output "Found $number1, $number2, and $remainder"
+                return [int]$number1 * [int]$number2 * [int]$remainder
             }
-            else {
-                $sum = [int]$number1 + [int]$number2 + [int]$number3
-                if ($sum -eq 2020){
-                    Write-Output "Found $number1 and $number2 and $number3"
-                    return [int]$number1 * [int]$number2 * [int]$number3
-                }
-            }
+
         }
+
     }
 }
+
 
