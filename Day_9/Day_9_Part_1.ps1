77a547d58bb3e70adbfb50997f33b9400373d49c
Alex Massey <rsjankis@gmail.com>
2020-12-09 20:26:29 -0600

    Initial commit

---
+++ Day_9/Day_9_Part_1.ps1
@@ -0,0 +1,48 @@
+##
+## https://adventofcode.com/2020/day/9
+##
+
+$inputData = Get-Content $PSScriptRoot\input.txt
+
+$numList = [System.Collections.Generic.List[System.Object]]($inputData[0..24])
+
+$startingIndex = 25
+$sums = @{}
+
+function Update-SumsTable {
+    param (
+        [object]$NumberList,
+        [hashtable]$SumsTable
+    )
+
+    foreach ($number in $NumberList) {
+        $numIndex = $NumberList.IndexOf($number)
+        for ($i = 0; $i -lt $NumberList.Count; $i++) {
+            if ($i -eq $numIndex) {
+                continue
+            }
+            $sumString1 = "$number,$($NumberList[$i])"
+            $sumString2 = "$($NumberList[$i]),$number"
+            if ($SumsTable.contains($sumString1) -or $SumsTable.Contains($sumString2)) {
+                continue
+            }
+            else {
+                $SumsTable."$number,$($NumberList[$i])" = [int]$number + [int]$NumberList[$i]
+            }
+        }
+    }
+}
+
+for ($i = $startingIndex; $i -lt $inputData.Length; $i++) {
+    $num = $inputData[$i]
+    Write-SumsTable -NumberList $numList -SumsTable $sums
+    if ($sums.Values -contains $num) {
+    }
+    else {
+        Write-Output "$num is invalid"
+        break
+    }
+    $numList.RemoveAt(0)
+    $numList.Add($num)
+}
+
