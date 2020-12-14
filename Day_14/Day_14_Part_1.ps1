0b5828b500b04fb3537af964f685b1bb5e5d0e06
Alex Massey <rsjankis@gmail.com>
2020-12-14 14:25:00 -0600

    Create Day_14_Part_1.ps1

---
+++ Day_14/Day_14_Part_1.ps1
@@ -0,0 +1,34 @@
+##
+## https://adventofcode.com/2020/day/9
+##
+
+$inputData = Get-Content $PSScriptRoot\input.txt
+
+$mem = @{}
+
+foreach ($line in $inputData){
+    if ($line -match "mask"){
+        $mask = $line.split()[-1]
+    } elseif ($line -match "mem") {
+        $memAddr = $line.split(" = ")[0].split("[]")[1]
+        $memValue = $line.split(" = ")[-1]
+        $binValue = [System.Convert]::ToString($memValue,2)
+        if ($binValue.Length -lt $mask.Length) {
+            $diff = $mask.Length - $binValue.Length
+            $binValue = ("0" * $diff) + $binValue
+        }
+
+        $writeValue = ""
+        for ($i = 0; $i -lt $mask.Length; $i++) {
+            if ($mask[$i] -eq "X") {
+                $writeValue += $binValue[$i]
+            }
+            else {
+                $writeValue += $mask[$i]
+            }
+        }
+        $mem.$memAddr = [System.Convert]::ToInt64($writeValue, 2)
+    }
+}
+
+($mem.values | Measure-Object -Sum).Sum
